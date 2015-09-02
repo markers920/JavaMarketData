7c7da3945b4bb9919c6aa77d9a9f6d6734ce42bc
Mark McDermott <markers920@gmail.com>
2015-09-02 16:32:16 +0000

    clean up cong

---
+++ src/main/java/markprojects/App.java
@@ -0,0 +1,13 @@
+package markprojects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
